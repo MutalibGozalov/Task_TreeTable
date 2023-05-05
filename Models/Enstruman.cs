a9d0bcb6dc18f1d232b45bfd4903526ee320b87b
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-05-05 11:43:31 +0400

    first commit

---
+++ Models/Enstruman.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass.Models;
+
+public abstract class Enstruman
+{
+    public  Enstruman(string? brand, string? model, decimal price)
+    {
+        Id = Guid.NewGuid();
+        Brand = brand;
+        Model = model;
+        Price = price;
+    }
+
+    public Guid Id { get; set; }
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public decimal Price { get; set; }
+
+    public abstract void DefaultSes();
+}
+
+public class Piano : Enstruman
+{
+    public string? Length { get; set; }
+    
+    public Piano(string? length, string? brand, string? model, decimal price) : base(brand, model, price)
+    {
+        Id = Guid.NewGuid();
+        Length = length;
+    }
+
+    public override void DefaultSes()
+    {
+        Console.WriteLine("Piano sesi");
+    }
+    public override string ToString()
+    {
+        return base.Brand;
+    }
+}
+
+public class Kenam : Enstruman
+{
+    public Kenam(string? arse, string? brand, string? model, decimal price) : base(brand, model, price)
+    {
+        Id = Guid.NewGuid();
+        this.Arse = arse;
+    }
+
+    public string Arse { get; set; }
+    public override void DefaultSes()
+    {
+        Console.WriteLine("Keman -))))");
+    }
+    public override string ToString()
+    {
+        return base.Brand;
+    }
+}
+
+public class Gitar : Enstruman
+{
+    public Gitar(string? magneticArray, string? brand, string? model, decimal price) : base(brand, model, price)
+    {
+        Id = Guid.NewGuid();
+        this.MagneticArray = magneticArray;
+    }
+
+    public string MagneticArray { get; set; }
+    public override void DefaultSes()
+    {
+        Console.WriteLine("Gitar sound -))))");
+    }
+    public override string ToString()
+    {
+        return base.Brand;
+    }
+}
+
+public class Flute : Enstruman
+{
+    public Flute(string? material, string? brand, string? model, decimal price) : base(brand, model, price)
+    {
+        Id = Guid.NewGuid();
+        this.Material = material;
+    }
+
+    public string Material { get; set; }
+    public override void DefaultSes()
+    {
+        Console.WriteLine("Flute sound -))))");
+    }
+    public override string ToString()
+    {
+        return base.Brand;
+    }
+}
+
+public class Muzisyen
+{
+    public Muzisyen(string? firstName, string? lastName, Enstruman alet)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        this.alet = alet;
+    }
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public Enstruman alet { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿
+using AbstractClass.Models;
+using Spectre.Console;
+using System.Collections;
+using System.Net.Http.Headers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Piano piano = new("9'8", "Yamaha", "Yamaha CFX Akustik Tam Kuyruklu Piyano", 388232100M);
+        Kenam keman = new Kenam(arse: "HaoYun HV92", brand: "Yamaha", model: "YSV13141", price: 38.03400M);
+        Gitar gitar = new Gitar("mgntc array", "Yamaha", "WLHJ76", 8800.97M);
+        Flute flute = new Flute("Plastic", "Yamaha", "HV21", 456.45M);
+
+        piano.DefaultSes();
+
+        Muzisyen pionist = new("Vladislav", "Spiltslav", piano);
+        Muzisyen kemanci = new("Hamza", "Kemansoy", keman);
+        Muzisyen gitarci = new("John", "Smith", gitar);
+        Muzisyen flutcu = new("Jmaes", "Frano", flute);
+
+        ArrayList muzisyenler = new ArrayList();
+        muzisyenler.Add(pionist);
+        muzisyenler.Add(gitarci);
+        muzisyenler.Add(flutcu);
+        muzisyenler.Add(kemanci);
+
+        ArrayList enstrumanlar = new ArrayList();
+        enstrumanlar.Add(piano);
+        enstrumanlar.Add(keman);
+        enstrumanlar.Add(gitar);
+        enstrumanlar.Add(flute);
+
+
+
+
+        // Muzisyen Tables
+        var musizyenTable = new Table();
+
+        foreach (var p in typeof(Muzisyen).GetProperties().OrderBy(x => x.Name))
+        {
+            musizyenTable.AddColumn(p.Name);
+           
+        }
+        foreach (var muzisyen in muzisyenler)
+        {
+            var pr = muzisyen.GetType().GetProperties().OrderBy(x => x.Name);
+            List<string> proplist = new List<string>();
+            foreach (var v in pr)
+                proplist.Add(v.GetValue(muzisyen).ToString());
+
+            musizyenTable.AddRow(proplist.ToArray());
+        }
+
+
+        // Create the tree
+        var root = new Tree("RNET102");
+
+        // Add some nodes
+        var foo = root.AddNode("[yellow]Musizyen[/]");
+        var table2 = foo.AddNode(musizyenTable);
+
+        // Render the tree
+        AnsiConsole.Write(root);
+
+
+
+        //Enstruman tables
+        var enstrumanTable = new Table();
+
+        foreach (var p in typeof(Kenam).GetProperties().OrderBy(x => x.Name))
+        {
+            enstrumanTable.AddColumn(p.Name);
+
+        }
+        foreach (var enstruman in enstrumanlar)
+        {
+            var pr = enstruman.GetType().GetProperties().OrderBy(x => x.Name);
+            List<string> proplist = new List<string>();
+            foreach (var v in pr)
+                proplist.Add(v.GetValue(enstruman).ToString());
+
+            enstrumanTable.AddRow(proplist.ToArray());
+        }
+
+
+        // Create the tree
+        var root2 = new Tree("RNET102");
+
+        // Add some nodes
+        var foo2 = root2.AddNode("[yellow]Enstruman[/]");
+        var table3 = foo2.AddNode(enstrumanTable);
+
+        // Render the tree
+        AnsiConsole.Write(root2);
+
+
+    }
+}
